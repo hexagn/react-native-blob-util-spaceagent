0e86f84d774e383c3c2a36978d8f3d6cb14b522d
Amritpal <45561952+TheAmritpal@users.noreply.github.com>
2025-05-12 14:20:02 +0530

    Update ReactNativeBlobUtilPackage.java

--- android/src/main/java/com/ReactNativeBlobUtil/ReactNativeBlobUtilPackage.java
+++ android/src/main/java/com/ReactNativeBlobUtil/ReactNativeBlobUtilPackage.java
@@ -2,30 +2,35 @@ package com.ReactNativeBlobUtil;
 
 import androidx.annotation.Nullable;
 
-import com.facebook.react.BaseReactPackage;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfo;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
+import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 // trick autolinking till it is fixed on RN side
 //public class ReactNativeBlobUtilPackage extends TurboReactPackage {
-public class ReactNativeBlobUtilPackage extends BaseReactPackage {
+public class ReactNativeBlobUtilPackage implements ReactPackage {
 
-    @Nullable
     @Override
-    public NativeModule getModule(String name, ReactApplicationContext reactContext) {
-        if (name.equals(ReactNativeBlobUtilImpl.NAME)) {
-            return new ReactNativeBlobUtil(reactContext);
-        } else {
-            return null;
-        }
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new ReactNativeBlobUtil(reactContext));
+        return modules;
     }
 
     @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
     public ReactModuleInfoProvider getReactModuleInfoProvider() {
         return () -> {
             final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
